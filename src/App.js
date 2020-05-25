a9c4df419ae0cabc92c251261e704c27a393832a
Retina <retina@PT-LTP-011-MAC.local>
2020-05-25 10:38:56 +0530

    added headers

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ const App = () => (
       menuitemsprops={`color: white; font-size: 20px; background-color: black; width: 350px`}
       tagprops={`color: white; background-color: black; font-size:10px`}
       searchprops={`width: 60px`}
+      header={`Customized Multi Selector with menu items, tag, search`}
     />
     <MyMultiSelector
       tagprops={`color: white; background-color: #660033; font-size:12px`}
       searchprops={`width: 200px`}
+      header={`Customized Multi Selector with tag, search`}
     />
   </div>
 );
